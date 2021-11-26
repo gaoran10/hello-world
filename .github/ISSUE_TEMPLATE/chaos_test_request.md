9c8182a9cc604d599b428d351e3bc8ae2c766a97
gaoran10 <gaoran_10@126.com>
2021-11-27 03:36:26 +0800

    add chaos test workflow

---
+++ .github/ISSUE_TEMPLATE/chaos_test_request.md
@@ -0,0 +1,14 @@
+---
+name: Chaos test request
+about: Open a chaos test
+title: ''
+labels: type/chaos-test
+assignees: ''
+---
+
+This issue is used to create and track a chaos test.
+
+== Chaos Test Arguments Header ==
+```
+EXTERNAL_HOST:xxxxxx
+```
---
+++ scripts/gen-workflows.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import subprocess
+
+
+class ChaosTestManager:
+
+    configuration = {}
+
+    def github_api_query_comments(self):
+        url = 'https://api.github.com/repos/gaoran10/hello-world/issues/{}/comments?per_page=100'.format(os.getenv('ISSUE_NUMBER'))
+        prefix = 'curl -H "Accept: application/vnd.github.v3+json" -u {}:{} '.format(os.getenv('GITHUB_USERNAME'), os.getenv('GITHUB_TOKEN'))
+        proc = subprocess.run(prefix + url, shell=True, capture_output=True)
+
+        return json.loads(proc.stdout.decode('utf-8'))
+
+    def get_chaos_test_configurations(self, comments):
+        lasthead = None
+        for comment in comments:
+            if comment['body'].startswith('== Chaos Test Arguments Header =='):
+                lasthead = comment['body']
+
+        if not lasthead:
+            print('The arguments not generated yet')
+            return
+
+        for line in lasthead.splitlines():
+            if not line.count(':'):
+                continue
+
+            var, val = line.split(':')[0], ':'.join(line.split(':')[1:])
+
+            self.configuration[var] = val
+
+
+def main():
+    os.environ['GITHUB_USERNAME'] = 'gaoran10'
+    os.environ['GITHUB_TOKEN'] = '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'
+    os.environ['ISSUE_NUMBER'] = '3'
+
+    chaos_test_manager = ChaosTestManager()
+    comments = chaos_test_manager.github_api_query_comments()
+    chaos_test_manager.get_chaos_test_configurations(comments)
+    print('configurations: ', chaos_test_manager.configuration)
+
+main()
