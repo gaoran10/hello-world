e4dd345c8aa6f2404065915d15e7a1f993865ee5
gaoran10 <gaoran_10@126.com>
2021-11-28 13:01:51 +0800

    fix chaos test workflow

--- scripts/chaos-test-workflow.py
+++ scripts/chaos-test-workflow.py
@@ -16,23 +16,27 @@ class ChaosTestManager:
 
         return json.loads(proc.stdout.decode('utf-8'))
 
-    def get_chaos_test_configurations(self, comments):
-        lasthead = None
-        for comment in comments:
-            if comment['body'].startswith('== Chaos Test Arguments Header =='):
-                lasthead = comment['body']
+    def get_chaos_test_configurations(self, comment_body):
+        configuration_body = None
+        if comment_body.startswith('== Chaos Test Configuration =='):
+            configuration_body = comment_body
 
-        if not lasthead:
+        if not configuration_body:
             print('The arguments not generated yet')
             return
 
-        for line in lasthead.splitlines():
-            if not line.count(':'):
+        boolean read_finish = False
+        json_str = ''
+        for line in configuration_body.splitlines():
+            if line.startswith('```json'):
                 continue
-
-            var, val = line.split(':')[0], ':'.join(line.split(':')[1:])
-
-            self.configuration[var] = val
+            if line.startswith('```'):
+                break
+            json_str += line
+        print('get chaos test json_str: ', json_str)
+        config = json.dumps(json_str, indent=4, separators=(',', ': '))
+        print('get chaos test config: ', config)
+        return config
 
     def github_api_create_comment(self, number, body):
         body = body.replace('\n', '\\n')
@@ -43,17 +47,46 @@ class ChaosTestManager:
         proc = subprocess.run(prefix + url + param, shell=True, capture_output=True)
         return json.loads(proc.stdout.decode('utf-8'))
 
-    def link_action_with_issue(self):
-        body = 'comment link action \n'
-        body = 'https://github.com/gaoran10/hello-world/actions/runs/' + os.getenv('RUN_ID')
-        print('link_action_with_issue action: ', body)
-        self.github_api_create_comment(os.getenv('ISSUE_NUMBER'), body)
+    def github_api_update_comment(self, comment_id, body):
+        body = body.replace('\n', '\\n')
+        url = 'https://api.github.com/repos/gaoran10/hello-world/issues/comments/{}'.format(comment_id)
+        prefix = 'curl -X PATCH -H "Accept: application/vnd.github.v3.text+json" -u {}:{} '.format(os.getenv('GITHUB_USERNAME'), os.getenv('GITHUB_TOKEN'))
+        param = ' -d \'{"body": "' + body + '"}\''
+
+        proc = subprocess.run(prefix + url + param, shell=True, capture_output=True)
+        return json.loads(proc.stdout.decode('utf-8'))
+
+    def github_api_get_comment(self, comment_id):
+        body = body.replace('\n', '\\n')
+        url = 'https://api.github.com/repos/gaoran10/hello-world/issues/comments/{}'.format(comment_id)
+        prefix = 'curl -H "Accept: application/vnd.github.v3.text+json" -u {}:{} '.format(os.getenv('GITHUB_USERNAME'), os.getenv('GITHUB_TOKEN'))
+
+        proc = subprocess.run(prefix + url + param, shell=True, capture_output=True)
+        return json.loads(proc.stdout.decode('utf-8'))
+
+    def link_action_with_issue(self, comment_id, test_action, old_comment_body):
+        body = old_comment_body
+        body += '\n'
+        body += '-------------- \n'
+        body += 'action: {} \n'.format(action)
+        if test_action == 'finish':
+            body += 'status: {} \n'.format(os.getenv('STATUS')
+        if test_action == 'create':
+            body += 'https://github.com/gaoran10/hello-world/actions/runs/' + os.getenv('RUN_ID')
+        print('link_action_with_issue body: ', body)
+        self.github_api_update_comment(comment_id, body)
 
 def main():
     chaos_test_manager = ChaosTestManager()
-    comments = chaos_test_manager.github_api_query_comments()
-    chaos_test_manager.get_chaos_test_configurations(comments)
-    print('configurations: ', chaos_test_manager.configuration)
-    chaos_test_manager.link_action_with_issue()
-
+    test_action = os.getenv('TEST_ACTION')
+    comment_body = os.getenv('COMMENT_BODY')
+    comment_id = os.getenv('COMMENT_ID')
+    if test_action == 'create':
+        print('chaos test create ...')
+        chaos_test_manager.get_chaos_test_configurations(comment_body)
+        chaos_test_manager.link_action_with_issue(comment_id, test_action, comment_body)
+    elif test_action = 'finish':
+        print('chaos test finish ...')
+        comment = chaos_test_manager.github_api_get_comment(comment_id)
+        chaos_test_manager.link_action_with_issue(comment_id, test_action, comment.body)
 main()
