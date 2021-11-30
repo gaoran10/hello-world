b5ccc824a5a5d2036947a66de11c748124278c01
gaoran10 <gaoran_10@126.com>
2021-12-01 02:41:38 +0800

    fix

--- scripts/chaos-cluster-manager.py
+++ scripts/chaos-cluster-manager.py
@@ -31,8 +31,8 @@ class ChaosClusterManager:
                 continue
 
             var, val = line.split(':')[0], ':'.join(line.split(':')[1:])
-            print('add chaos cluster configuration in env. ', var.strip(), '=', var.strip()
             os.environ['CHAOS_CLUSTER_' + var.strip()] = val.strip()
+            print('add chaos cluster configuration in env. ', var.strip(), '=', var.strip())
 
     def github_api_update_issue(self, issue_number, body):
         url = 'https://api.github.com/repos/gaoran10/hello-world/issues/{}'.format(issue_number)
