5a2b386d69880ab6a69f445e5c225be8a1353467
gaoran10 <gaoran_10@126.com>
2022-03-07 01:43:40 +0800

    adjust test run manually logic

--- scripts/chaos-test-workflow-manually2.py
+++ scripts/chaos-test-workflow-manually2.py
@@ -17,21 +17,31 @@ def main():
         pulsar_proxy_external_ip = subprocess.os.popen("kubectl get svc -n chaos-" + os.getenv('CLUSTER_ID') + " | grep proxy | awk '{print$4}'").read().strip()
         print('pulsar proxy external ip: ', pulsar_proxy_external_ip)
 
-        if os.getenv('TEST_NAME') == 'KoPTest':
+        test_command = os.getenv('TEST_COMMAND')
+        test_command += " -Dpulsar.deployment.type=EXTERNAL"
+        test_command += " -Dpulsar.external.service.domain=" + pulsar_proxy_external_ip
+        test_command += " -Dchaos.test.duration=" + str(os.getenv('TEST_DURATION'))
+        test_command += " -Dchaos.test.id=" + test_id
+        test_command += " -Dchaos.test-platform.server=" + server_hook_url
+
+        if os.getenv('TEST_NAME').startswith('KoP'):
             print('start build kop test docker image')
             os.system('docker build -t kop-test -f chaos-test/kop-docker/KoPTestDockerfile .')
-            print('run kop test docker image')
-            test_res = os.system('docker run -e istioIP=' + istio_external_ip + ' -e clusterId=' + os.getenv('CLUSTER_ID') + ' -e pulsarIP=' + pulsar_proxy_external_ip + ' kop-test')
+            print('run kop test based on docker image')
+
+            test_command += "-Dchaos.test.istio.external.ip=chaos-pulsar-" + os.getenv('CLUSTER_ID') + "-broker-0.pulsar.kop.service"
+            command = "docker run"
+            command += " -e ISTIO_IP=" + istio_external_ip
+            command += " -e CLUSTER_ID=" + os.getenv('CLUSTER_ID')
+            command += " -e PULSAR_IP=" + pulsar_proxy_external_ip
+            command += " -e TEST_COMMAND=" + test_command
+            command += " kop-test"
+            print('run kop test command: ', command)
+            test_res = os.system(command)
             if test_res != 0:
                 raise RuntimeError("Chaos test for kop failed. code " + str(test_res) + ".")
         else:
-            command = "cd chaos-test && " + os.getenv('TEST_COMMAND')
-            command += " -Dpulsar.deployment.type=EXTERNAL"
-            command += " -Dpulsar.external.service.domain=" + pulsar_proxy_external_ip
-            command += " -Dchaos.test.duration=" + str(os.getenv('TEST_DURATION'))
-            command += " -Dchaos.test.istio.external.ip=" + istio_external_ip
-            command += " -Dchaos.test.id=" + test_id
-            command += " -Dchaos.test-platform.server=" + server_hook_url
+            command = "cd chaos-test && " + test_command
             print('run test command: ', command)
             test_res = os.system(command)
             if test_res != 0:
